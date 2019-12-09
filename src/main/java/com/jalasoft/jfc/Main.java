e07c63d3ac7f7af0bba097850fa74fde8ea05389
Enrique <Enrique.Carrizales@fundacion-jala.org>
2019-12-08 20:08:12 -0400

    Add main method

--- src/main/java/com/jalasoft/jfc/Main.java
+++ src/main/java/com/jalasoft/jfc/Main.java
@@ -1,6 +1,10 @@
 package com.jalasoft.jfc;
 
 public class Main {
+    /**
+     * This static method is the project main
+     * @param args
+     */
     public static void main(String[] args){
         System.out.println("Hello World");
     }
