7aac18c4ab01926c22d30407a2cacf6d9f492abf
Enrique <Enrique.Carrizales@fundacion-jala.org>
2019-12-09 19:38:25 -0400

    update changes

--- src/main/java/com/jalasoft/jfc/Main.java
+++ src/main/java/com/jalasoft/jfc/Main.java
@@ -1,11 +1,13 @@
 
-/*******************************************************************************
- * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- ******************************************************************************/
+/**
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 
 package com.jalasoft.jfc;
 
@@ -13,13 +15,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author Luis
+ * @author Enrique Carrizales
  */
 @SpringBootApplication
 public class Main {
 
     /**
-     * Main method print a message
+     * Print a message
      * @param args receives String array
      */
     public static void main(String[] args) {
