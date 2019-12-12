4721b0369b1c33c1161d3bee453d7957072a8314
Admin <alan.escalera@fundacion-jala.org>
2019-12-12 17:23:04 -0400

    add the Class Param

---
+++ src/main/java/com/jalasoft/jfc/Param.java
@@ -0,0 +1,10 @@
+package com.jalasoft.jfc;
+public class Param {
+  /**
+   * This Class is the class father
+   */
+  private String InputPathFile;   //Input path file
+  private String OutputPathFile;  //Output path file
+  private String outputFileName;  //Name the file that will be converted
+
+}
