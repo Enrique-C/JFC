b920fd533dfd908670cc4345849ec8acf858910e
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2019-12-12 18:29:58 -0400

    Fix comments

--- src/main/java/com/jalasoft/jfc/Main.java
+++ src/main/java/com/jalasoft/jfc/Main.java
@@ -7,21 +7,24 @@
  * accordance with the terms of the license agreement you entered into
  * with Jalasoft.
  */
+
 package com.jalasoft.jfc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /***
+ * Executes Sprint Boot to Web Application
+ * @version 0.1
  * @author Enrique Carrizales
  */
 @SpringBootApplication
 public class Main {
     /**
-     * Main method
+     * Main method executes SprintApplication
      * @param args receives String array
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Wellcome to JFC");
         SpringApplication.run(Main.class, args);
     }
