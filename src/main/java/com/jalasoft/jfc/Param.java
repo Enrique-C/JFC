78e29e73ff1667969e37b9d780a2c0fd56e18208
Admin <alan.escalera@fundacion-jala.org>
2019-12-13 10:14:20 -0400

    fixed the documentation observed

--- src/main/java/com/jalasoft/jfc/Param.java
+++ src/main/java/com/jalasoft/jfc/Param.java
@@ -1,8 +1,27 @@
+/*
+ *
+ *  Copyright (c) 2019 Jalasoft.
+ *
+ *  This software is the confidential and proprietary information of Jalasoft.
+ *  ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into
+ *  with Jalasoft.
+ * /
+ */
+
 package com.jalasoft.jfc;
+
+/**
+*
+*  Param Class.
+*
+* @version 1.0  Dic 2019
+*
+* @author Alan Escalera
+*/
 public class Param {
-  /**
-   * This Class is the class father.
-   */
+
   private String InputPathFile;   //Input path file
   private String OutputPathFile;  //Output path file
   private String outputFileName;  //Name the file that will be converted
