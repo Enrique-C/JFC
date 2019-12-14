f8e914ae70998e9214b7779a1dbe907c26d9ddb8
Enrique <Enrique.Carrizales@fundacion-jala.org>
2019-12-14 15:58:23 -0400

    Add VideoConverterController class

--- src/main/java/com/jalasoft/jfc/controller/VideoConverterController.java
+++ src/main/java/com/jalasoft/jfc/controller/VideoConverterController.java
@@ -15,29 +15,73 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
- *  Manage VideoConvert Requests.
+ *  Manage VideoConverter Requests.
  *
  * @version 0.1
  *
  * @author Enrique Carrizales
  */
 @RestController
-@RequestMapping(path = "/convertVideo")
+@RequestMapping(path = "/videoConverter")
 public class VideoConverterController {
 
     private static final String UPLOADED_FOLDER =
             "src/main/java/com/jalasoft/jfc/resources/"; //Constant upload file.
 
     /**
-     *
-     * @return
+     * videoConverter method receives an image to convert
+     * @param file contains the image file.
+     * @param fFmpeg binary variable of FFmpeg.
+     * @param inputPathFile contains the input path of the image.
+     * @param outputPathFile contains the output path of image converted.
+     * @param outputFileName contains name of converted image.
+     * @param aspectRatio contains aspect ratio value.
+     * @param frameRate contains the number of images per second.
+     * @param wight contains video's wight.
+     * @param height contains video's height.
+     * @param videoCodec contains videoCodec value.
+     * @param audioCodec contains audioCodec value.
+     * @param videoBitRate contains videoBitRate value.
+     * @param audioBitRate contains audioBitRate value.
+     * @param quality contains quality of video.
+     * @param channelsNumber contains number of output channels.
+     * @param volume contains the level of sound.
+     * @param rotate degrees of rotation.
+     * @return get the path of the upload file.
      */
     @PostMapping
-    public String videoConvert(
-            @RequestParam("file")MultipartFile file,
-            @RequestParam String fFmpeg
-            ){
-        return "";
+    public String videoConverter(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam String fFmpeg,
+            @RequestParam String inputPathFile,
+            @RequestParam String outputPathFile,
+            @RequestParam String outputFileName,
+            @RequestParam double aspectRatio,
+            @RequestParam String frameRate,
+            @RequestParam int wight,
+            @RequestParam int height,
+            @RequestParam String videoCodec,
+            @RequestParam String audioCodec,
+            @RequestParam String videoBitRate,
+            @RequestParam String audioBitRate,
+            @RequestParam byte quality,
+            @RequestParam byte channelsNumber,
+            @RequestParam String volume,
+            @RequestParam String rotate) {
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return UPLOADED_FOLDER;
     }
 }
