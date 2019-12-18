2f771575c763d0385e3383d0256967aa6c87b273
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2019-12-18 17:00:28 -0400

    Add ICommandStrategy

---
+++ src/main/java/com/jalasoft/jfc/model/strategy/ICommandStrategy.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.jfc.model.strategy;
+
+/**
+ *
+ * ICommandStrategy interface defines behavior to Commands classes.
+ *
+ * @version 0.1 18 Dic 2019.
+ *
+ * @author Enrique Carrizales.
+ */
+public interface ICommandStrategy {
+
+    /**
+     * Builds a command.
+     * @param value receives a param.
+     * @return String of a command.
+     */
+    public String command(String value);
+}
