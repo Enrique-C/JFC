0b324d70d9c6476b75f4ca74e17ee6a1486306bd
Enrique <Enrique.Carrizales@fundacion-jala.org>
2019-12-17 23:41:14 -0400

    Test binary Image Magick

--- src/main/java/com/jalasoft/jfc/model/image/ImageConverter.java
+++ src/main/java/com/jalasoft/jfc/model/image/ImageConverter.java
@@ -13,6 +13,8 @@ import com.jalasoft.jfc.model.FileResult;
 import com.jalasoft.jfc.model.IConverter;
 import com.jalasoft.jfc.model.Param;
 
+import java.io.IOException;
+
 /**
  * Converts a image type to another.
  *
@@ -29,13 +31,22 @@ public class ImageConverter implements IConverter {
      */
     public FileResult convert(Param param) {
 
-        ImageParam imageParam = (ImageParam)param;
-        String IMAGE_MAGIC_PATH = "C:\\Users\\Luis\\Downloads\\ImageMagick-7.0.9-9-portable-Q16-x64\\magick.exe";
-
-        String convertCommand = "magick ";
-
-
+        imageConverter(param);
 
         return null;
+    }
+
+    private void imageConverter(Param param) {
+
+        String IMAGE_MAGIC_PATH = "C:\\Users\\Luis\\Downloads\\ImageMagick-7.0.9-9-portable-Q16-x64\\magick.exe";
+        String convertCommand = " magick ";
+
+        try {
+            ProcessBuilder process = new ProcessBuilder(IMAGE_MAGIC_PATH, convertCommand, param.getInputPathFile(), param.getOutputFileName());
+            String prueba = process.toString();
+            Process p = process.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
