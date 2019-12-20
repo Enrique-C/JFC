ea0fe8d3afdd045040259994a2032193f24564bb
Oscar Lopez <oscar.lopez@fundacion-jala.org>
2019-12-20 16:46:07 -0400

    model/fix-convert-exception

---
+++ src/main/java/com/jalasoft/jfc/model/strategy/CommandVideoAspectRatio.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.jfc.model.strategy;
+
+import com.jalasoft.jfc.model.video.VideoCommand;
+
+public class CommandVideoAspectRatio implements ICommandStrategy{
+
+    // Content command value.
+    private String commandValue;
+
+    /**
+     * This is the constructor of class.
+     *
+     * @param commandValue, receive a value.
+     * @return command concatenated.
+     */
+    public CommandVideoAspectRatio(String commandValue) {
+        this.commandValue = commandValue;
+    }
+
+    /**
+     * This method builds a command.
+     *
+     * @return command concatenated.
+     */
+    @Override
+    public String command() {
+        return this.SPACE + VideoCommand.INFILE + this.SPACE + commandValue + this.SPACE;
+    }
+}
