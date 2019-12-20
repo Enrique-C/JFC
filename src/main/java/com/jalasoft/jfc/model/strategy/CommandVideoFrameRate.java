861f1ca5112db0784be50ee9d25d8ab5b5d1e679
Oscar Lopez <oscar.lopez@fundacion-jala.org>
2019-12-20 18:07:49 -0400

    Implement Strategy classes

---
+++ src/main/java/com/jalasoft/jfc/model/strategy/CommandVideoFrameRate.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.jfc.model.strategy;
+
+/**
+ * This Class change the Frame Rate of a video.
+ *
+ * @version 0.1 20 Dic 2019.
+ *
+ * @author Oscar Lopez.
+ */
+public class CommandVideoFrameRate implements ICommandStrategy {
+
+    // Content command value.
+    private String commandValue;
+
+    /**
+     * Creates a new convert object.
+     * @param commandValue, receive a value.
+     * @return command concatenated.
+     */
+    public CommandVideoFrameRate(String commandValue) {
+        this.commandValue = commandValue;
+    }
+
+    /**
+     * This method builds a command.
+     * @return command concatenated.
+     */
+    @Override
+    public String command() {
+        return this.SPACE + commandValue + this.SPACE;
+    }
+}
