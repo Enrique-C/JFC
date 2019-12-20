6c825202ca069624939bf82ffad026bdf8cd0dd5
juanMartinez <juan.martinez@fundacion-jala.org>
2019-12-20 16:23:20 -0400

    Command resize class refactored

--- src/main/java/com/jalasoft/jfc/model/strategy/CommandResize.java
+++ src/main/java/com/jalasoft/jfc/model/strategy/CommandResize.java
@@ -14,40 +14,37 @@ import com.jalasoft.jfc.model.pdf.ImageMagickCommand;
 /**
  * This class validates width and height.
  *
- * @version 0.1 19 Dic 2019
- *
  * @author Juan Martinez
+ * @version 0.1 19 Dic 2019
  */
 public class CommandResize implements ICommandStrategy {
 
-    // Content command value.
-    private String commandValue;
+    // Content width value.
+    private int width;
+
+    // Content height value.
+    private int height;
 
     /**
-     * It Creates a new CommandResize object.
-     * @param commandValue contains a value.
-     */
-    public CommandResize(String commandValue) {
-        this.commandValue = commandValue;
-    }
-
-    /**
-     * This method builds a command.
-     * @return command concatenated.
-     */
-    public String command() {
-        return SPACE;
-    }
-
-    /**
-     * This method builds a command.
+     * Allows to instantiate CommandResize.
+     *
      * @param width, height receive a value.
      * @return command concatenated.
      */
-    public String command(int width, int height) {
-        if (width > 0 && height > 0){
+    public CommandResize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * This method builds a command.
+     *
+     * @return command concatenated.
+     */
+    public String command() {
+        if (width > 0 && height > 0) {
             return SPACE + ImageMagickCommand.RESIZE.getCommand() +
-                   SPACE + width + ImageMagickCommand.ASTERISK + height;
+                    SPACE + width + ImageMagickCommand.ASTERISK + height;
         }
         return null;
     }
