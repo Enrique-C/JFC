ec0d744a34853f5b3c34cf7673c97437b28a8053
juanMartinez <juan.martinez@fundacion-jala.org>
2019-12-23 11:25:57 -0400

    zipFolder class added

---
+++ src/main/java/com/jalasoft/jfc/model/utility/ZipFolder.java
@@ -0,0 +1,9 @@
+package com.jalasoft.jfc.model.utility;
+
+import java.io.File;
+
+public class ZipFolder {
+    public void zipFolderFile(final File [] files, final File pathZipFileName){
+        
+    }
+}
