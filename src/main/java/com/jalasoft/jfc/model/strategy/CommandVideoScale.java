ebe46fdd96f07d312cad81ad6cdba9409806fd0c
Oscar Lopez <oscar.lopez@fundacion-jala.org>
2019-12-23 09:14:23 -0400

    Strategy Video Scale

---
+++ src/main/java/com/jalasoft/jfc/model/strategy/CommandVideoScale.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.jfc.model.strategy;
+
+import com.jalasoft.jfc.model.pdf.ImageMagickCommand;
+import com.jalasoft.jfc.model.video.VideoCommand;
+
+/**
+ * This Class change Command Video Scale of a video.
+ *
+ * @version 0.1 23 Dic 2019.
+ *
+ * @author Oscar Lopez.
+ */
+public class CommandVideoScale implements ICommandStrategy {
+
+    // Content command value.
+    private int width;
+    private int height;
+
+    /**
+     * Creates a new CommandVideoScale object.
+     *
+     * @param width, receive a value.
+     * @param height, receive a value.
+     */
+    public CommandVideoScale(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * This method builds a command.
+     *
+     * @return command concatenated.
+     */
+    @Override
+    public String command() {
+        if (width > 0 && height > 0) {
+            return SPACE + VideoCommand.SCALE.getCommand() + SPACE + width + VideoCommand.COLON.getCommand() + height;
+        }
+        return null;
+    }
+}
