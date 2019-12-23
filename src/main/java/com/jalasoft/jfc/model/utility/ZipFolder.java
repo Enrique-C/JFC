dc3b2da0753f5eee7b26676c75ce6b5d94b1694b
juanMartinez <juan.martinez@fundacion-jala.org>
2019-12-23 14:18:14 -0400

    ZipFolder class improved

--- src/main/java/com/jalasoft/jfc/model/utility/ZipFolder.java
+++ src/main/java/com/jalasoft/jfc/model/utility/ZipFolder.java
@@ -23,6 +23,25 @@ import java.util.zip.ZipOutputStream;
 public class ZipFolder {
 
     /**
+     * This method adds existent file to zip file.
+     * @param currentFile is the existent file.
+     * @param zipOutputStream is the stream content.
+     * @throws IOException is thrown when there are some error.
+     */
+    public void addCurrentFile(File currentFile, ZipOutputStream zipOutputStream) throws IOException {
+        int quantity = 128;
+        byte [] bufferBytes = new byte[quantity];
+        ZipEntry entry = new ZipEntry(currentFile.getName());
+        FileInputStream fileInputStream = new FileInputStream(currentFile);
+        zipOutputStream.putNextEntry(entry);
+        int read = 0;
+        while ((read = fileInputStream.read(bufferBytes)) != -1) {
+            zipOutputStream.write(bufferBytes, 0, read);
+        }
+        zipOutputStream.closeEntry();
+        fileInputStream.close();
+    }
+    /**
      * This method compress files.
      * @param files content a list of files.
      * @param pathZipFileName content path name of zip file.
@@ -32,19 +51,10 @@ public class ZipFolder {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(pathZipFileName);
             ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
-            int quantity = 128;
-            byte [] bufferBytes = new byte[quantity];
+
             for (File currentFile : files) {
                 if (!currentFile.isDirectory()) {
-                    ZipEntry entry = new ZipEntry(currentFile.getName());
-                    FileInputStream fileInputStream = new FileInputStream(currentFile);
-                    zipOutputStream.putNextEntry(entry);
-                    int read = 0;
-                    while ((read = fileInputStream.read(bufferBytes)) != -1) {
-                        zipOutputStream.write(bufferBytes, 0, read);
-                    }
-                    zipOutputStream.closeEntry();
-                    fileInputStream.close();
+                    addCurrentFile(currentFile, zipOutputStream);
                 }
             }
             zipOutputStream.close();
