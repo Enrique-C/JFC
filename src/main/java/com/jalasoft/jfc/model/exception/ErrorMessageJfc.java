793299aa2b16c82d4196a48f63716eaffe258d9e
Admin <alan.escalera@fundacion-jala.org>
2020-01-06 16:44:43 -0400

    observations fixed

--- src/main/java/com/jalasoft/jfc/model/exception/ErrorMessageJfc.java
+++ src/main/java/com/jalasoft/jfc/model/exception/ErrorMessageJfc.java
@@ -17,9 +17,9 @@ package com.jalasoft.jfc.model.exception;
  * @author Alan Escalera
  */
 public enum ErrorMessageJfc {
-    INVALID_INPUT_PATH("Invalid input file path value\n"),
+    INVALID_INPUT_PATH("Invalid file path input\n"),
     FILE_NO_FOUND("File not found\n"),
-    PAGES_NULL("Pages to convert value is NULL "),
+    PAGES_NULL("Value of pages to convert is NULL"),
     PAGES_NUMBER_NOT_EXIST("Number of page doesn't exist"),
     INVALID_SCALE("Invalid input file path value\n"),
     EXIFTOOL_NOT_EXIST("Exiftool doesn't exist\n"),
@@ -37,8 +37,8 @@ public enum ErrorMessageJfc {
     RESIZE_INVALID("Invalid resize values\n"),
     WIDTH_HEIGHT_INVALID("Width or height are invalid\n"),
     IMAGE_ROTATE_INVALID("Invalid Image rotate value\n"),
-    COMMAND_NULL("Command value is NULL "),
-    COMMAND_INVALID("command value is invalid\n");
+    COMMAND_NULL("Command value is NULL"),
+    COMMAND_INVALID("Command value is invalid\n");
 
     // Command variable.
     private String message;
