4fd8e36552c4cc6a3cd593b844d1e186e75cb713
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2020-01-06 18:58:17 -0400

    Fix observation and update FolderRemover class

--- src/test/java/com/jalasoft/jfc/model/image/ImageConverterTest.java
+++ src/test/java/com/jalasoft/jfc/model/image/ImageConverterTest.java
@@ -11,6 +11,7 @@ package com.jalasoft.jfc.model.image;
 
 import com.jalasoft.jfc.model.exception.CommandValueException;
 import com.jalasoft.jfc.model.exception.ConvertException;
+import com.jalasoft.jfc.model.utility.FolderRemover;
 import com.jalasoft.jfc.model.utility.PathJfc;
 import org.junit.Test;
 
@@ -31,12 +32,27 @@ public class ImageConverterTest {
         File zipFile = new File(zipImage);
         boolean expected = zipFile.exists() && zipFile.getTotalSpace() > EMPTY_BYTES;
 
+        FolderRemover.removeFolder(zipFile.getPath());
+
         assertTrue(expected);
     }
 
     @Test
-    public void convert_ImagePNBToJPGAndThumbnailAndMetadata() {
-        //Todo
+    public void convert_ImagePNBToJPGAndThumbnailAndMetadata() throws ConvertException, CommandValueException {
+        File fileUpload = new File("fileUploadedPath");
+
+        ImageConverter imageConverter = new ImageConverter();
+        ImageParam imageParam = generateImageJPGAndThumbnailAndMetadata();
+
+        String zipImage = imageConverter.convert(imageParam).getDownload();
+        final long UPLOAD_FILE_BYTES = fileUpload.getTotalSpace();
+
+        File zipFile = new File(zipImage);
+        boolean expected = zipFile.exists() && zipFile.getTotalSpace() > UPLOAD_FILE_BYTES;
+
+        FolderRemover.removeFolder(zipFile.getPath());
+
+        assertTrue(expected);
     }
 
     private ImageParam generateOnlyImageJPG() {
@@ -44,14 +60,45 @@ public class ImageConverterTest {
 
         String fileUploadedPath = "src/test/resources/fundacionjala.png";
 
-        File fileUpload = new File("fileUploadedPath");
-
         String md5 = "af738d53637a29f531fa0c2bf1eb1516";
         String outputPath = "src/test/resources/";
         String outputName = "img";
         String imageFormat = ".jpg";
         boolean isThumbnail = false;
         boolean isMetadata = false;
+        boolean isGrayscale = false;
+        int ImageWidth = 0;
+        int ImageHeight = 0;
+        float degreesToRotate = 0;
+
+        ImageParam imageParam = new ImageParam();
+        imageParam.setMd5(md5);
+        imageParam.setInputPathFile(fileUploadedPath);
+        imageParam.setOutputPathFile(outputPath);
+        imageParam.setImageFormat(imageFormat);
+        imageParam.setOutputName(outputName);
+        imageParam.isThumbnail(isThumbnail);
+        imageParam.isMetadata(isMetadata);
+        imageParam.isGrayscale(isGrayscale);
+        imageParam.setImageWidth(ImageWidth);
+        imageParam.setImageHeight(ImageHeight);
+        imageParam.setDegreesToRotate(degreesToRotate);
+        imageParam.setFolderName(md5);
+
+        return imageParam;
+    }
+
+    private ImageParam generateImageJPGAndThumbnailAndMetadata() {
+        PathJfc pathJfc = new PathJfc();
+
+        String fileUploadedPath = "src/test/resources/fundacionjala.png";
+
+        String md5 = "af738d53637a29f531fa0c2bf1eb1516";
+        String outputPath = "src/test/resources/";
+        String outputName = "img";
+        String imageFormat = ".jpg";
+        boolean isThumbnail = true;
+        boolean isMetadata = true;
         boolean isGrayscale = false;
         int ImageWidth = 0;
         int ImageHeight = 0;
