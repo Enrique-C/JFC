5469d361973bd2bcc3be2a1c0aea076467c419b6
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-06 10:26:57 -0400

    imports updated in download controller

--- src/main/java/com/jalasoft/jfc/controller/DownloadController.java
+++ src/main/java/com/jalasoft/jfc/controller/DownloadController.java
@@ -13,12 +13,16 @@ import com.jalasoft.jfc.model.utility.PathJfc;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletContext;
 
