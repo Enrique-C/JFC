bfeb3f45ee7801855d856bceea22cd9201b4063d
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-07 14:36:50 -0400

    comment in test class

---
+++ src/test/java/com/jalasoft/jfc/controller/PdfConverterControllerTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.jfc.controller;
+
+/**
+ * Used for testing PdfConverterController.
+ *
+ * @version 0.1 07 Jan 2020.
+ *
+ * @author Juan Martinez.
+ */
+public class PdfConverterControllerTest {
+    // to do.
+}
