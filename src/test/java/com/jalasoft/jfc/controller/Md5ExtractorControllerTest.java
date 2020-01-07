84583a81a8a3d6b42d098c544d5fc42eb0ca85f8
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-07 19:07:36 -0400

    Md5Extractor test implemented for testing endpoint

--- src/test/java/com/jalasoft/jfc/controller/Md5ExtractorControllerTest.java
+++ src/test/java/com/jalasoft/jfc/controller/Md5ExtractorControllerTest.java
@@ -10,11 +10,13 @@
 package com.jalasoft.jfc.controller;
 
 import com.jalasoft.jfc.Main;
+import org.apache.pdfbox.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +25,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes= Main.class)
@@ -34,20 +39,19 @@ public class Md5ExtractorControllerTest {
 
     @Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(wContext)
-                .alwaysDo(MockMvcResultHandlers.print())
-                .build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wContext).alwaysDo(MockMvcResultHandlers.print()).build();
     }
 
     @Test
     public void extract_Md5OfFile_md5String() throws Exception {
-        String text ="C:/Users/Admin/IdeaProjects/JFC/src/test/resources/pdf.pdf";
+        File filePath = new File("C:/Users/Admin/IdeaProjects/JFC/src/test/resources/pdf.pdf");
+        FileInputStream input = new FileInputStream(filePath);
+
         String expected = "8bd6509aba6eafe623392995b08c7047";
-        MockMultipartFile file = new MockMultipartFile("file","pdf.pdf",
-                "application/pdf", text.getBytes());
-        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/extractMd5/")
-                .file(file)
-                .characterEncoding("UTF-8"))
-                .andExpect(MockMvcResultMatchers.content().string(expected));
+
+        MockMultipartFile file = new MockMultipartFile("file",filePath.getName(),
+                MediaType.APPLICATION_PDF_VALUE, IOUtils.toByteArray(input));
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/extractMd5/").file(file)
+                .characterEncoding("UTF-8")).andExpect(MockMvcResultMatchers.content().string(expected));
     }
 }
