ac6d91ccc6ec74ca05a1bca294464613fcf2a0ee
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-06 20:07:28 -0400

    pdfconverter class updated

--- src/main/java/com/jalasoft/jfc/model/pdf/PdfConverter.java
+++ src/main/java/com/jalasoft/jfc/model/pdf/PdfConverter.java
@@ -39,7 +39,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.ZipException;
 
 /**
  * This class converts PDF to Image
