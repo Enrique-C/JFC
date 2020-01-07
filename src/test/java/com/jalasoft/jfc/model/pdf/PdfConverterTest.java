91abeccb91ad11c1b685e024285a836debb49eeb
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-06 20:13:30 -0400

    happy test implemented in pdf converter test class

--- src/test/java/com/jalasoft/jfc/model/pdf/PdfConverterTest.java
+++ src/test/java/com/jalasoft/jfc/model/pdf/PdfConverterTest.java
@@ -9,16 +9,16 @@
 
 package com.jalasoft.jfc.model.pdf;
 
-import com.jalasoft.jfc.controller.PdfConverterController;
+import com.jalasoft.jfc.model.exception.CommandValueException;
+import com.jalasoft.jfc.model.exception.ConvertException;
+import com.jalasoft.jfc.model.exception.ZipJfcException;
 import com.jalasoft.jfc.model.utility.PathJfc;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Executes pdfConvert's unit tests.
@@ -29,31 +29,186 @@ import java.io.InputStream;
  */
 public class PdfConverterTest {
 
-    private InputStream is;
-    private MockMvc mockMvc;
-    private PdfConverterController controller = new PdfConverterController();
-    @Before
-    public void init() {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        is = controller.getClass().getClassLoader().getResourceAsStream("excel.xlsx");
+    @Test
+    public void convert_PdfFileToImage() throws ConvertException, ZipJfcException, CommandValueException, IOException {
+        PdfConverter pdfConverter = new PdfConverter();
+        PdfParam pdfParam = generatePdfParamObject();
+        String zipPdfFile = pdfConverter.convert(pdfParam).getDownload();
+        final int EMPTY_BYTES = 0;
+        File zipFileGenerated = new File(zipPdfFile);
+        boolean expected = zipFileGenerated.exists() && zipFileGenerated.getTotalSpace() > EMPTY_BYTES;
+
+        assertTrue(expected);
     }
 
     @Test
-    public void convertPdfFileToImage() {
+    public void convert_PdfFileToImageWithThumbnail() throws ConvertException, ZipJfcException, CommandValueException,
+            IOException {
         PdfConverter pdfConverter = new PdfConverter();
-        PdfParam pdfParam = new PdfParam();
-        pdfParam.setInputPathFile("src/test/resources/pdf.pdf");
-//        pdfParam.setOutputPathFile();
+        PdfParam pdfParam = generatePdfParamObjectThumbnail();
+        String zipPdfFile = pdfConverter.convert(pdfParam).getDownload();
+        final int EMPTY_BYTES = 0;
+        File zipFileGenerated = new File(zipPdfFile);
+        boolean expected = zipFileGenerated.exists() && zipFileGenerated.getTotalSpace() > EMPTY_BYTES;
+
+        assertTrue(expected);
+    }
+
+    @Test
+    public void convert_PdfFileToImageWithMetadata() throws ConvertException, ZipJfcException, CommandValueException,
+            IOException {
+        PdfConverter pdfConverter = new PdfConverter();
+        PdfParam pdfParam = generatePdfParamObjectMetadata();
+        String zipPdfFile = pdfConverter.convert(pdfParam).getDownload();
+        final int EMPTY_BYTES = 0;
+        File zipFileGenerated = new File(zipPdfFile);
+        boolean expected = zipFileGenerated.exists() && zipFileGenerated.getTotalSpace() > EMPTY_BYTES;
+        
+        assertTrue(expected);
+    }
+
+    @Test(expected = Exception.class)
+    public void convertPdfFileToImageWithNullObjectValue() throws ConvertException, ZipJfcException,
+            CommandValueException, IOException {
+            PdfConverter pdfConverter = new PdfConverter();
+            PathJfc pathJfc = new PathJfc();
+            PdfParam pdfParam = null;
+            String zipPdfFile = pdfConverter.convert(pdfParam).getDownload();
     }
 
     private PdfParam generatePdfParamObject() {
         PathJfc pathJfc = new PathJfc();
         String pathOfUploadedFile = "src/test/resources/pdf.pdf";
-        File fileUploaded = new File(pathOfUploadedFile);
         String outputPath = "src/test/resources/";
-        String outputName = "fileone";
+        String outputName = "fileConverted";
         String imageFormat = ".jpg";
         String pagesToConvert = "2";
+        int quantityOfPages = 4;
+        int width = 0;
+        int height = 0;
+        String scale = "%";
+        int rotate = 0;
+        String md5 = "8bd6509aba6eafe623392995b08c7047";
+        boolean isThumbnail = false;
+        boolean isMetadata = false;
 
+        PdfParam pdfParam = new PdfParam();
+        pdfParam.setMd5(md5);
+        pdfParam.setInputPathFile(pathOfUploadedFile);
+        pdfParam.setOutputPathFile(outputPath);
+        pdfParam.setPagesToConvert(pagesToConvert);
+        pdfParam.setQuantityOfPage(quantityOfPages);
+        pdfParam.setHeight(height);
+        pdfParam.setWidth(width);
+        pdfParam.setScale(scale);
+        pdfParam.setRotate(rotate);
+        pdfParam.setImageFormat(imageFormat);
+        pdfParam.setOutputName(outputName);
+        pdfParam.setThumbnail(isThumbnail);
+        pdfParam.setFolderName(md5);
+        pdfParam.isMetadata(isMetadata);
+        return pdfParam;
+    }
+
+    private PdfParam generatePdfParamObjectThumbnail() {
+        PathJfc pathJfc = new PathJfc();
+        String pathOfUploadedFile = "src/test/resources/pdf.pdf";
+        String outputPath = "src/test/resources/";
+        String outputName = "filethumbnail";
+        String imageFormat = ".jpg";
+        String pagesToConvert = "2";
+        int quantityOfPages = 4;
+        int width = 0;
+        int height = 0;
+        String scale = "%";
+        int rotate = 0;
+        String md5 = "8bd6509aba6eafe623392995b08c7047";
+        boolean isThumbnail = true;
+        boolean isMetadata = false;
+
+        PdfParam pdfParam = new PdfParam();
+        pdfParam.setMd5(md5);
+        pdfParam.setInputPathFile(pathOfUploadedFile);
+        pdfParam.setOutputPathFile(outputPath);
+        pdfParam.setPagesToConvert(pagesToConvert);
+        pdfParam.setQuantityOfPage(quantityOfPages);
+        pdfParam.setHeight(height);
+        pdfParam.setWidth(width);
+        pdfParam.setScale(scale);
+        pdfParam.setRotate(rotate);
+        pdfParam.setImageFormat(imageFormat);
+        pdfParam.setOutputName(outputName);
+        pdfParam.setThumbnail(isThumbnail);
+        pdfParam.setFolderName(md5);
+        pdfParam.isMetadata(isMetadata);
+        return pdfParam;
+    }
+
+    private PdfParam generatePdfParamObjectMetadata() {
+        PathJfc pathJfc = new PathJfc();
+        String pathOfUploadedFile = "src/test/resources/pdf.pdf";
+        String outputPath = "src/test/resources/";
+        String outputName = "filemetadata";
+        String imageFormat = ".png";
+        String pagesToConvert = "2";
+        int quantityOfPages = 4;
+        int width = 0;
+        int height = 0;
+        String scale = "%";
+        int rotate = 0;
+        String md5 = "8bd6509aba6eafe623392995b08c7047";
+        boolean isThumbnail = false;
+        boolean isMetadata = true;
+
+        PdfParam pdfParam = new PdfParam();
+        pdfParam.setMd5(md5);
+        pdfParam.setInputPathFile(pathOfUploadedFile);
+        pdfParam.setOutputPathFile(outputPath);
+        pdfParam.setPagesToConvert(pagesToConvert);
+        pdfParam.setQuantityOfPage(quantityOfPages);
+        pdfParam.setHeight(height);
+        pdfParam.setWidth(width);
+        pdfParam.setScale(scale);
+        pdfParam.setRotate(rotate);
+        pdfParam.setImageFormat(imageFormat);
+        pdfParam.setOutputName(outputName);
+        pdfParam.setThumbnail(isThumbnail);
+        pdfParam.setFolderName(md5);
+        pdfParam.isMetadata(isMetadata);
+        return pdfParam;
+    }
+
+    private PdfParam generatePdfParamObjectNull() {
+        PathJfc pathJfc = new PathJfc();
+        String pathOfUploadedFile = null;
+        String outputPath = null;
+        String outputName = null;
+        String imageFormat = ".png";
+        String pagesToConvert = "2";
+        int quantityOfPages = 4;
+        int width = 0;
+        int height = 0;
+        String scale = "%";
+        int rotate = 0;
+        String md5 = null;
+        boolean isThumbnail = false;
+        boolean isMetadata = true;
+
+        PdfParam pdfParam = new PdfParam();
+        pdfParam.setMd5(md5);
+        pdfParam.setInputPathFile(pathOfUploadedFile);
+        pdfParam.setOutputPathFile(outputPath);
+        pdfParam.setPagesToConvert(pagesToConvert);
+        pdfParam.setQuantityOfPage(quantityOfPages);
+        pdfParam.setHeight(height);
+        pdfParam.setWidth(width);
+        pdfParam.setScale(scale);
+        pdfParam.setRotate(rotate);
+        pdfParam.setImageFormat(imageFormat);
+        pdfParam.setOutputName(outputName);
+        pdfParam.setThumbnail(isThumbnail);
+        pdfParam.setFolderName(md5);
+        pdfParam.isMetadata(isMetadata);
+        return pdfParam;
     }
 }
