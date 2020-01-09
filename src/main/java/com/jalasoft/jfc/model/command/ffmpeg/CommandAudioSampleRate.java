42a0b08044372e06c8bdbf92dc1227dc0595aa04
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2020-01-08 20:14:22 -0400

    Adds of CommandAudioSampleRate class

---
+++ src/main/java/com/jalasoft/jfc/model/command/ffmpeg/CommandAudioSampleRate.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
+package com.jalasoft.jfc.model.command.ffmpeg;
+
+import com.jalasoft.jfc.model.audio.AudioFfmpegCommand;
+import com.jalasoft.jfc.model.command.ICommandStrategy;
+import com.jalasoft.jfc.model.exception.CommandValueException;
+import com.jalasoft.jfc.model.exception.ErrorMessageJfc;
+
+public class CommandAudioSampleRate implements ICommandStrategy {
+
+    // Contents sampleRate value.
+    private short audioSampleRate;
+
+    /**
+     * Creates a new CommandAudio object.
+     * @param audioSampleRate receives values.
+     */
+    public CommandAudioSampleRate(short audioSampleRate) {
+        this.audioSampleRate = audioSampleRate;
+    }
+
+    /**
+     * Builds commands.
+     * @return commands concatenated.
+     * @throws CommandValueException when there is an invalid command.
+     */
+    @Override
+    public String command() throws CommandValueException {
+        getSampleRate();
+
+        return this.SPACE + AudioFfmpegCommand.AUDIO_SAMPLE_RATE.getFfmpegCommand() + this.SPACE + audioSampleRate
+                + AudioFfmpegCommand.KBPS.getFfmpegCommand();
+    }
+
+    /**
+     * Reviews audioSampleRate value.
+     * @throws CommandValueException when there is an invalid value.
+     */
+    private void getSampleRate() throws CommandValueException {
+        final short DEFAULT = (short)44100;
+
+        if (audioSampleRate == 0) {
+            audioSampleRate = DEFAULT;
+        }
+        if (audioSampleRate > DEFAULT) {
+            throw new CommandValueException(ErrorMessageJfc.SAMPLE_RATE_EXCEEDED.getErrorMessageJfc() + ": "
+                    + audioSampleRate, this.getClass().getName());
+        }
+    }
+}
