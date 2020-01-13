abb004704acc2f9aebbe404a0dcb5cfa97dd1019
juanMartinez <juan.martinez@fundacion-jala.org>
2020-01-13 11:38:36 -0400

    observations fixed

--- src/main/java/com/jalasoft/jfc/controller/ImageConverterController.java
+++ src/main/java/com/jalasoft/jfc/controller/ImageConverterController.java
@@ -27,8 +27,8 @@ import com.jalasoft.jfc.model.utility.PathJfc;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
 import io.swagger.annotations.Authorization;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
