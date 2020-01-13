ddaa9a65f2fadc5fac664b557da53c37f78a728d
Oscar Lopez <oscar.lopez@fundacion-jala.org>
2020-01-13 08:55:44 -0400

    Implemantacion Hibernate on Video Controller V4

--- src/main/java/com/jalasoft/jfc/controller/MetadataConverterController.java
+++ src/main/java/com/jalasoft/jfc/controller/MetadataConverterController.java
@@ -10,10 +10,12 @@
 package com.jalasoft.jfc.controller;
 
 import com.jalasoft.jfc.model.Param;
+import com.jalasoft.jfc.model.entity.FileEntity;
 import com.jalasoft.jfc.model.exception.ConvertException;
 import com.jalasoft.jfc.model.exception.Md5Exception;
 import com.jalasoft.jfc.model.exception.ZipJfcException;
 import com.jalasoft.jfc.model.metadata.MetadataConverter;
+import com.jalasoft.jfc.model.repository.FileRepository;
 import com.jalasoft.jfc.model.result.FileResponse;
 import com.jalasoft.jfc.model.result.MessageResponse;
 import com.jalasoft.jfc.model.result.Response;
@@ -27,6 +29,7 @@ import com.jalasoft.jfc.model.utility.ZipFolder;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +61,9 @@ public class MetadataConverterController {
      * @param request is client request value.
      * @return ResponseEntity<Response> for status code.
      */
+    // Inject FileRepository
+    @Autowired
+    FileRepository fileRepository;
     @PostMapping("/metadataConverter")
     @ApiOperation(value = "File", notes = "Provides values for converting metadata",
             response = Response.class)
@@ -70,6 +76,17 @@ public class MetadataConverterController {
             String fileUploaded = FileServiceController.writeFile(pathJfc.getInputFilePath() +
             file.getOriginalFilename(), file);
             Param param = new Param();
+
+            FileEntity fileEntity = new FileEntity();
+
+            if (fileRepository.findByMd5(Md5Checksum.getMd5(fileUploaded)) != null) {
+                param.setInputPathFile(fileRepository.findByMd5(Md5Checksum.getMd5(fileUploaded)).getFilePath());
+            } else {
+                param.setInputPathFile(fileUploaded);
+                fileEntity.setFilePath(fileUploaded);
+                fileEntity.setMd5(Md5Checksum.getMd5(fileUploaded));
+                fileRepository.save(fileEntity);
+            }
             param.setInputPathFile(fileUploaded);
             param.setOutputPathFile(PathJfc.getOutputFilePath());
             param.setFolderName(Md5Checksum.getMd5(fileUploaded));
