86622d389d1936ff27b8127babfcefd571c77c34
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2020-01-14 17:33:23 -0400

    Seconds changes

--- src/test/java/com/jalasoft/jfc/controller/AudioConverterControllerTest.java
+++ src/test/java/com/jalasoft/jfc/controller/AudioConverterControllerTest.java
@@ -19,13 +19,11 @@ import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -54,13 +52,7 @@ public class AudioConverterControllerTest {
         FileInputStream input = new FileInputStream(filePath);
 
         MockMultipartFile file = new MockMultipartFile("file", filePath.getName(),
-                MediaType.APPLICATION_OCTET_STREAM_VALUE, IOUtils.toByteArray(input));
-
-
-        /*MockMultipartFile file = new MockMultipartFile("file", filePath.getName(),
-                "text/plain", IOUtils.toByteArray(input));
-
-         */
+                null, IOUtils.toByteArray(input));
 
         mockMvc.perform(MockMvcRequestBuilders.fileUpload("/api/audioConverter/").file(file)
                 .param("md5", "882559480156e9cddf65ed3125521b9922"))
